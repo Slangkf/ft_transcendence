18e08d47bcec2398bee3303374b092ea82f21b6c
tclouet <tclouet@42paris.fr>
2026-04-08 15:35:26 +0200

    modification of the onsubmit syntax which is depreciated

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -2,7 +2,7 @@
 	<div class="flex flex-col justify-center w-full max-w-80 rounded-xl px-6 py-8 border border-slate-700 bg-slate-900/90 backdrop-blur-xs text-white text-sm">
 		<h2 class="text-2xl font-semibold text-pink-500 text-center">Login</h2>
 		<p class="mt-1 text-pink-500 text-center">Sign in or create an account</p>
-		<form on:submit|preventDefault={handleSubmit} class="mt-8">
+		<form onsubmit={handleSubmit} class="mt-8">
 			<label for="email" class="block mb-1 font-medium text-pink-500">Email address</label>
 			<input type="email" id="email" name="email" placeholder="Email" class="w-full p-2 mb-3 bg-slate-900 border border-slate-700 rounded-md focus:outline-none focus:ring-1 transition focus:ring-indigo-500 focus:border-indigo-500">
 			
@@ -10,7 +10,7 @@
 			<input type="password" id="password" name="password" placeholder="Password" class="w-full p-2 mb-2 bg-slate-900 border border-slate-700 rounded-md focus:outline-none focus:ring-1 transition focus:ring-indigo-500 focus:border-indigo-500">
 			
 			<div class="text-right">
-				<a href="#" class="font-medium text-blue-500 hover:text-pink-500">Forgot password?</a>
+				<a href="/login" class="font-medium text-blue-500 hover:text-pink-500">Forgot password?</a>
 			</div>
 
 			<button type="submit" class="w-full mt-10 px-4 py-2.5 font-medium text-slate-200 bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-indigo-500">Login</button>
@@ -21,6 +21,7 @@
 <!-- Handle form submit: prevent reload and send POST request to backend -->
 <script>
 	async function handleSubmit(event) {
+		event.preventDefault();
 		const form = event.target;
 		const email = form.email.value;
 		const password = form.password.value;
