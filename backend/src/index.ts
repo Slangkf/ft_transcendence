540d757ab82dd2c987e9d0820c1415379a826615
jmen <jmen@f1br9s7.paris.42.school>
2026-04-09 17:10:48 +0200

    setting prisma good, problem in index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,12 @@
-import {express} from 'express';
+import express from 'express';
 import {Request, Response } from 'express'
-import {audRouter} from 'src/authendification/aud.router.js'; 
+import {audRouter} from 'authendification/aud.router.js'; 
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use('', audRouter);
+app.use('/', audRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
