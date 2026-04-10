8329dc76b6bc02854f1cb9bc465c881e2eba21a3
Thomas Clouet <clouet_thomas@icloud.com>
2026-04-10 11:30:31 +0200

    Add files via upload

---
+++ frontend/src/routes/game/+page.svelte
@@ -0,0 +1,3 @@
+<div class="text-center text-4xl text-white">
+	GAME PAGE
+</div>
---
+++ frontend/src/routes/profile/+page.ts
@@ -0,0 +1,8 @@
+// export async function load() {
+// 	const response = await fetch('/api/profile');
+// 	const user = await response.json();
+
+// 	return {
+// 		user
+// 	};
+// }
