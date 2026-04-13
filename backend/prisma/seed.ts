9c4bf3273f72a2cd359aa1a2ac92afb0255820be
Igor Kayibanda <ikayiban@f4r4s7.clusters.42paris.fr>
2026-04-13 13:52:05 +0200

    modif docker compose pour ubuntu + ajout de tous les questionnaires

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   seed.ts                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmen <jmen@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: ikayiban <ikayiban@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/02 18:30:48 by ikayiban          #+#    #+#             */
-/*   Updated: 2026/04/10 11:49:48 by jmen             ###   ########.fr       */
+/*   Updated: 2026/04/11 14:24:22 by ikayiban         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,39 +47,46 @@ async function main(): Promise<void> {
             score: 0
         }
     });
-    
-    const dataDir: string = path.join(__dirname, "data");
-    
-    if (!fs.existsSync(dataDir))
-    {
-        console.error("directory not found!");
-        return ;
-    }
 
-    const files: string[] = fs.readdirSync(dataDir).filter(f => f.endsWith('.json'));
+    const folders: string[] = [
+        "culture_generale",
+        "divertissement",
+        "histoire_geo",
+        "sciences"
+    ];
 
-    for (const file of files)
-    {
-        const filePath: string = path.join(dataDir, file);
-        const content: Quiz = JSON.parse(fs.readFileSync(filePath, 'utf-8')) as Quiz;
+    for (const folder of folders) {
+        const folderPath: string = path.join(__dirname, "data", folder);
 
-        await prisma.quiz.create({
-            data: {
-                title: content.title,
-                category: content.category,
-                authorId: admin.id,
-                questions: {
-                    create: content.questions.map((q: Question) => ({
-                        text: q.text,
-                        options: q.options,
-                        answer: q.answer
-                    }))
+        if (!fs.existsSync(folderPath)) {
+            console.warn(`Dossier non trouvé : ${folderPath}`);
+            continue;
+        }
+
+        const files: string[] = fs.readdirSync(folderPath).filter(f => f.endsWith('.json'));
+
+        for (const file of files) {
+            const filePath: string = path.join(folderPath, file);
+            const content: Quiz = JSON.parse(fs.readFileSync(filePath, 'utf-8')) as Quiz;
+
+            await prisma.quiz.create({
+                data: {
+                    title: content.title,
+                    category: content.category,
+                    authorId: admin.id,
+                    questions: {
+                        create: content.questions.map((q: Question) => ({
+                            text: q.text,
+                            options: q.options,
+                            answer: q.answer
+                        }))
+                    }
                 }
-            }
-        });
+            });
+            console.log(`good : ${folder}/${file}`);
+        }
     }
 }
-
 main()
   .catch((e: Error) => {
     console.error(e);
