21ae2f1cb9bacc338c469a54b5a08f3928fd14c2
Thomas Clouet <clouet_thomas@icloud.com>
2026-04-22 13:06:28 +0200

    Add files via upload

---
+++ frontend/src/lib/shared/user.schema.ts
@@ -0,0 +1,49 @@
+import {z} from 'zod'
+
+//type user for database 
+export const User_DB = z.object({
+    id: z.string().uuid(),
+    username: z.string().min(3).max(20),
+    email: z.string().email(),
+    password: z.string(),
+    createdAt: z.date()
+})
+export type UserDB = z.infer<typeof User_DB>;
+
+//type data from front
+export const Register_Input = z.object({
+    username: z.string().min(3).max(20),
+    email: z.string().email(),
+    password: z.string().min(8).max(20)
+})
+export type RegisterInput = z.infer<typeof Register_Input>;
+
+//type for login
+export const Login_Input = z.object({
+    email: z.string().email(), 
+    password: z.string().min(8)
+})
+export type LoginInput = z.infer<typeof Login_Input>;
+
+//type output for front， without password
+export const User_Output = z.object({
+    id: z.string().uuid(),
+    username: z.string().min(3).max(20),
+    email: z.string().email()
+})
+export type UserOutput = z.infer<typeof User_Output>;
+
+//typpe to authendification
+export const Auth_result = z.object({
+    token: z.string(),
+    user: User_Output
+})
+export type AuthResult = z.infer<typeof Auth_result>;
+
+//type input for change password, only oldpassword and 2 new password
+export const Change_Pd_Input = z.object({
+    oldpassword: z.string().min(1),
+    newpassword: z.string().min(3).max(20),
+    confirmpd: z.string().min(3).max(20)
+})
+export type ChangePdInput = z.infer<typeof Change_Pd_Input>;
