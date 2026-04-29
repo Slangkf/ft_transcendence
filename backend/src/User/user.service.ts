734e8a80c7103a841ec6457bb4052e2e063264af
ikayiban <ikayiban@f4r3s7.paris.42.school>
2026-04-29 12:43:18 +0200

    ajout loggout, donnees profil, changement de username et upload d'avatar

--- backend/src/User/user.service.ts
+++ backend/src/User/user.service.ts
@@ -67,10 +67,11 @@ export class UserService{
 
     async update_avatar(userid: number, file?: Express.Multer.File): Promise<UserOutput>{
         if (!file) {
-            throw new AppError(
-                "avatar file is required",
-                ErrorCode.AVATAR_REQUIRED,
-                400);
+            // throw new AppError(
+            //     "avatar file is required",
+            //     ErrorCode.AVATAR_REQUIRED,
+            //     400);
+            throw new AppError("avatar file is required", 400);
         }
 
         const user = await this.userrepository.find_by_id(userid);
