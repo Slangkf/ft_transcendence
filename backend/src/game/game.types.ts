87573f02317b887124bbfc803d4bd5211d50dec4
jmen <jmen@f4r11s10.paris.42.school>
2026-05-11 15:50:07 +0200

    game type

--- backend/src/game/game.types.ts
+++ backend/src/game/game.types.ts
@@ -26,19 +26,11 @@ export interface Player{
     id: string;
     score: number;
     answers: PlayerAnswer[];
-<<<<<<< HEAD
-    status: "playing" | 'answered';
-=======
     status: PlayerStatus;
->>>>>>> alice
     Totaltime: number;
     isAI?: boolean;
-<<<<<<< HEAD
-    joinOrder?: number; // to get a host for the room 
-=======
     joinOrder?: number;
     nickname?: string;
->>>>>>> alice
 }
 
 
@@ -132,72 +124,9 @@ export type SetReadyResult = {
     gameresponse?: GameUpdateResponse;
 };
 
-<<<<<<< HEAD
-export type BaseGameState = {
-    gameId: string;
-    questions: Question[];
-    players: Record<string, Player>;
-    currentQuestionIndex: number;
-    isFinished: boolean;
-    startedAt: number;
-}
-
-export type SoloGameState = BaseGameState & {
-    mode: "solo" | "ai"
-}
-
-export type MultiGameState = BaseGameState & {
-    mode: "multiplayer";
-    roomId: string;
-    hostId: string;
-    status: "waiting" | "starting" | "playing" | "finished";
-}
-export type GameState = SoloGameState | MultiGameState;
-
-//type for front 
-export type PublicGameState = {
-  gameId: string;
-  players: Record<string, PublicPlayer>;
-  currentQuestionIndex: number;
-  isFinished: boolean;
-  totalQuestions: number;
-  mode: "solo" | "ai" | "multiplayer";
-  roomId?: string;
-  hostId?: string;
-  status?: "waiting" | "starting" | "playing" | "finished";
-}
-
-export type FinalScore = {
-    gameId: string;
-    players: Record<string, PublicPlayer>;
-    winner: string; //give the id of winner
-    finishedAt: number;
-}
-//give all info to front with the correct answer
-export type PlayingGameInfo = {
-    gameresult: PublicGameState,
-    correctAnswer: string,
-    nextQuestion: PublicQuestion | null,
-}
-
-export type FinishedGameInfo = {
-    gameresult: PublicGameState;
-    finalscore: FinalScore;
-}
-
-export type GameInfo = PlayingGameInfo | FinishedGameInfo; 
-
-export interface IGameRepository {
-    create(game:GameState): Promise<void>;
-    findById(gameId: string): Promise<GameState | null>;
-    update(game: GameState): Promise<void>;
-    delete(gameId: string): Promise<void>;
-}
-=======
 //input 
 export type StartGameParams = {
    mode: GameMode.SOLO | GameMode.MULTIPLAYER;
    userId: string;
    nickname: string;
 }
->>>>>>> alice
