623c02c3e4251ba6bce29ed80e720b553341bcf7
tclouet <tclouet@42paris.fr>
2026-05-14 15:05:27 +0200

    add token check in each route of the app

---
+++ frontend/src/lib/shared/user.schema.ts
@@ -0,0 +1,70 @@
+import {z} from 'zod'
+
+//type user for database 
+export const User_DB = z.object({
+    id: z.number().int(),
+    username: z.string().min(3).max(20),
+    email: z.string().email(),
+    password: z.string(),
+    createdAt: z.date(),
+    url: z.string().max(512).nullable().optional(),
+    score: z.number().int().nullable().optional(),
+    wins: z.number().int().nullable().optional(),
+    played: z.number().int().nullable().optional(),
+    friendsNb: z.number().int().nullable().optional(),
+    status: z.enum(['ONLINE', 'OFFLINE', 'AWAY', 'IN_GAME']).optional(),
+    role: z.enum(['USER', 'ADMIN']).optional()
+})
+export type UserDB = z.infer<typeof User_DB>;
+
+//type data from front
+export const Register_Input = z.object({
+    username: z.string().min(3).max(20),
+    email: z.string().email(),
+    password: z.string().min(8).max(20)
+})
+export type RegisterInput = z.infer<typeof Register_Input>;
+
+//type for login
+export const Login_Input = z.object({
+    email: z.string().email(), 
+    password: z.string().min(8)
+})
+export type LoginInput = z.infer<typeof Login_Input>;
+
+//type output for front， without password
+export const User_Output = z.object({
+    id: z.number().int(),
+    createdAt: z.date(),
+    username: z.string().min(3).max(20),
+    email: z.string().email(),
+    url: z.string().max(512).nullable().optional(),
+    score: z.number().int().nullable().optional(),
+    wins: z.number().int().nullable().optional(),
+    played: z.number().int().nullable().optional(),
+    friendsNb: z.number().int().nullable().optional(),    status: z.enum(['ONLINE', 'OFFLINE', 'AWAY', 'IN_GAME']).optional()
+})
+export type UserOutput = z.infer<typeof User_Output>;
+
+//typpe to authendification
+export const Auth_result = z.object({
+    token: z.string(),
+    user: User_Output
+})
+export type AuthResult = z.infer<typeof Auth_result>;
+
+//type input for change password, only oldpassword and 2 new password
+export const Change_Pd_Input = z.object({
+    newpassword: z.string().min(8, "Must be at least 8 characters").max(20, "Must not exceed 20 characters"),
+    confirmpd: z.string().min(8, "Must be at least 8 characters").max(20, "Must not exceed 20 characters")
+})
+.refine((data) => data.newpassword === data.confirmpd, {
+	path: ["confirmpd"],
+	message: "Passwords do not match",
+});
+export type ChangePdInput = z.infer<typeof Change_Pd_Input>;
+
+export const Change_Username_Input = z.object({
+   username: z.string().min(3, "Must be at least 3 characters").max(20, "Must not exceed 20 characters")
+})
+export type ChangeUsernameInput = z.infer<typeof Change_Username_Input>;
--- frontend/src/routes/profile/[username]/+page.server.ts
+++ frontend/src/routes/profile/[username]/+page.server.ts
@@ -5,6 +5,7 @@ import { error, redirect, isRedirect, isHttpError } from '@sveltejs/kit';
 export const load: PageServerLoad = async ({ cookies, fetch, params }) => {
 	// Redirect unauthenticated users to login
 	const token = cookies.get('auth_token');
+	console.log(token);
 	if (!token) {
 		console.error('cookies.get error in the profile/[username] section')
 		throw redirect(302, '/login');
