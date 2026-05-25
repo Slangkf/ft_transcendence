fa22d51a4f26a243d50435586a728481462e549b
tclouet <tclouet@42paris.fr>
2026-05-25 17:08:42 +0200

    websocket.schema file deleted

--- frontend/src/lib/shared/websocket.schema.ts
+++
@@ -1,8 +0,0 @@
-import {z} from 'zod'
-
-// Base schema used for all socket or API messages.
-// The `type` field identifies the kind of message being transmitted.
-export const Base_Message = z.object({
-    type: z.string(),
-    
-})
